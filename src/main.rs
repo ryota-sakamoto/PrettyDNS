8efa8211740981325844e03dfb283a6e11fe6ab2
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2021-04-26 02:41:37 +0900

    concurrent

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io;
-use tokio::net::UdpSocket;
+use std::{io, sync::Arc};
+use tokio::{net::UdpSocket, sync::mpsc};
 
 mod client;
 mod message;
@@ -7,22 +7,44 @@ mod message;
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let sock = UdpSocket::bind("0.0.0.0:53").await?;
+    let sock = Arc::new(sock);
+    let (tx, rx) = mpsc::channel::<(Vec<u8>, std::net::SocketAddr)>(1000);
+    tokio::spawn(handler(sock.clone(), rx));
+
     let mut buf = [0; 1024];
     loop {
         let (len, addr) = sock.recv_from(&mut buf).await?;
-
-        println!("---");
-        println!("data: {:?}", &buf[..len]);
-        println!("len: {:?}", len);
-
-        let req = message::from_bytes(&buf).await.unwrap();
-        println!("req: {:?}", req);
-
-        // tokio::spawn(async {
-        let result = client::dig(req).await?;
-        println!("result: {:?}", result);
-
-        sock.send_to(&result, addr).await?;
-        // });
+        tx.send((buf[..len].to_vec(), addr)).await.unwrap();
     }
+}
+
+async fn handler(
+    sock: Arc<UdpSocket>,
+    rx: mpsc::Receiver<(Vec<u8>, std::net::SocketAddr)>,
+) -> io::Result<()> {
+    let mut rx = rx;
+    while let Some((buf, addr)) = rx.recv().await {
+        tokio::spawn(_handler(sock.clone(), buf, addr));
+    }
+
+    return Ok(());
+}
+
+async fn _handler(
+    sock: Arc<UdpSocket>,
+    buf: Vec<u8>,
+    addr: std::net::SocketAddr,
+) -> io::Result<()> {
+    println!("---");
+    println!("data: {:?}", buf);
+
+    let req = message::from_bytes(&buf).await?;
+    println!("req: {:?}", req);
+
+    let result = client::dig(req).await?;
+    println!("result: {:?}", result);
+
+    sock.send_to(&result, addr).await?;
+
+    Ok(())
 }
