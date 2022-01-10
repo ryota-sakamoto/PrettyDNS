fad202754369e00e37ff42ad5ae2ad08c3a7744b
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2022-01-11 01:55:52 +0900

    add cache test

--- src/server/cache.rs
+++ src/server/cache.rs
@@ -6,7 +6,7 @@ use tracing::info;
 
 static CACHE: Lazy<Mutex<HashMap<String, Record>>> = Lazy::new(|| Mutex::new(HashMap::new()));
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Record {
     cached_at: DateTime<Utc>,
     data: Vec<Resource>,
@@ -50,4 +50,34 @@ pub fn cache(domain: String, data: Vec<Resource>) -> Result<(), ()> {
         },
     );
     return Ok(());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{cache, resolve};
+    use crate::message::{qtype::QType, resource::Resource};
+
+    #[tokio::test]
+    async fn test_resolve_none() {
+        let domain = "example.com.".to_owned();
+        let list = resolve(domain);
+        assert_eq!(list, None);
+    }
+
+    #[tokio::test]
+    async fn test_resolve_some() {
+        let domain = "example.com.".to_owned();
+        let resource = Resource {
+            name: vec![103, 111, 111, 103, 108, 101, 46, 99, 111, 109, 46],
+            _type: QType::A,
+            class: 1,
+            ttl: 299,
+            rdlength: 4,
+            rdata: vec![172, 217, 25, 238],
+        };
+        cache(domain.clone(), vec![resource]).unwrap();
+
+        let list = resolve(domain);
+        assert!(list.is_some());
+    }
 }
